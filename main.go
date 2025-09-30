b21b1033db10220456d75bcb19f1f2fdc01f0d17
Sidrit Trandafili <forge405@gmail.com>
2025-09-30 11:55:50 +0200

    stateful requests

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	message := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, _ = w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) resetHitsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	message := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, _ = w.Write([]byte(message))
+}
 
 func main() {
-	fmt.Println("chirp")
+	mux := http.NewServeMux()
+
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+
+	filePathRoot := http.Dir(".")
+	fs := http.FileServer(filePathRoot)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", apiCfg.hitsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHitsHandler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
